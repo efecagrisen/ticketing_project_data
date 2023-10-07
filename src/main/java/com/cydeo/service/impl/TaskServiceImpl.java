eb00c27b7b4be4d4a6cc96a6a2a70a599568eb23
efecagrisen <efecagrisen@yahoo.com>
2023-10-07 22:24:21 +0300

    Task create page

--- src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
@@ -1,11 +1,24 @@
 package com.cydeo.service.impl;
 
 import com.cydeo.dto.TaskDTO;
+import com.cydeo.mapper.TaskMapper;
+import com.cydeo.repository.TaskRepository;
 import com.cydeo.service.TaskService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class TaskServiceImpl implements TaskService {
+
+    private final TaskRepository taskRepository;
+    private final TaskMapper taskMapper;
+
+    public TaskServiceImpl(TaskRepository taskRepository, TaskMapper taskMapper) {
+        this.taskRepository = taskRepository;
+        this.taskMapper = taskMapper;
+    }
+
     @Override
     public List<TaskDTO> listAllTasks() {
         return null;
