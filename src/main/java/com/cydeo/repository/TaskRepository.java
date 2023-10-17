427c89be9da382c7366ab1b2f0ec2d91a44a4bc3
efecagrisen <efecagrisen@yahoo.com>
2023-10-17 19:58:24 +0300

    project delete method improved

--- src/main/java/com/cydeo/repository/TaskRepository.java
+++ src/main/java/com/cydeo/repository/TaskRepository.java
@@ -1,5 +1,6 @@
 package com.cydeo.repository;
 
+import com.cydeo.entity.Project;
 import com.cydeo.entity.Task;
 import org.hibernate.sql.Select;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -16,6 +17,8 @@ public interface TaskRepository extends JpaRepository<Task, Long> {
             "FROM tasks t JOIN projects p on t.project_id=p.id " +
             "WHERE p.project_code=?1 AND t.task_status='COMPLETE'",nativeQuery = true)
     int totalCompletedTasks(String projectCode);
+
+    List<Task> findAllByProject(Project project);
 
 
 
